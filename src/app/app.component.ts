ebedba68bf2bcee267abf7d8c3b0a9c740ef421a
vsheva <valera.sheva@gmail.com>
2023-11-27 23:56:12 -0500

    ngSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   numbers = [1, 2, 3, 4, 5];
-  oddNumbers = [1,3,5];
-  evenNumbers = [2,4]
+  oddNumbers = [1, 3, 5];
+  evenNumbers = [2, 4]
   onlyOdd = false;
+  value = 5;
 }
